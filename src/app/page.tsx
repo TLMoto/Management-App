5c78eaf98c189d668c9fb888c8145fcadbd36af5
Antonio Cardim <antonio.cardim@tecnico.ulisboa.pt>
2025-11-19 14:33:36 +0000

    Add Airtable integration and update dependencies in package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
 import Image from "next/image";
 
+/**
+ * 
+ * type User = {
+  id: string;
+  nome: string;
+  department: string;
+  funcao: string;
+  istId: number;
+};
+
+  const { user, setUser, department, setDepartment, funcao, setFuncao };
+  const hasPermission = (role: string): boolean => {
+    const regex = /^(Sublíder|Líder)(?: de .+)?$/;
+    return regex.test(role);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await UserService.getUser(99461);
+      setUser(data);
+      setDepartment(data?.department || "");
+      setFuncao(data?.funcao || "");
+    }, [department, user?.funcao]);
+    
+    fetchData();
+  }, [setUser, setDepartment]);
+
+ */
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
